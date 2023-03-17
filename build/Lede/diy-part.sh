1260ed88e562f7590c87a0457da251c32f7eff5b
emsvip <93297386+emsvip@users.noreply.github.com>
2023-03-17 15:44:52 +0800

    Create diy-part.sh

---
+++ build/Lede/diy-part.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# DIY扩展二合一了，在此处可以增加插件
+# 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
+# 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
+
+# 后台IP设置
+export Ipv4_ipaddr="192.168.2.1"            # 修改openwrt后台地址(填0为关闭)
+export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
+# export Op_name="OpenWrt-123"                # 修改主机名称为OpenWrt-123(填0为不作修改)
+
+# 默认主题设置
+export Mandatory_theme="argon"              # 将bootstrap替换您需要的主题为必选主题(可自行更改您要的,源码要带此主题就行,填写名称也要写对) (填写主题名称,填0为不作修改)
+export Default_theme="argon"                # 多主题时,选择某主题为默认第一主题 (填写主题名称,填0为不作修改)
+
+# 删除默认防火墙
+sed -i '/to-ports 53/d' "$ZZZ_PATH"
+
+# 个性签名,默认增加年月日[$(TZ=UTC-8 date "+%Y.%m.%d")]
+export Customized_Information="EmsVIP compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ_PATH"  # 个性签名,你想写啥就写啥，(填0为不作修改)
+
+# 更换固件内核
+export Replace_Kernel="0"                    # 更换内核版本,在对应源码的[target/linux/架构]查看patches-x.x,看看x.x有啥就有啥内核了(填入内核版本号,填0为不作修改)
+
+# 设置免密码登录(个别源码本身就没密码的)
+export Password_free_login="1"               # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为启用命令,填0为不作修改)
+
