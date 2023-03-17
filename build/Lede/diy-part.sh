c08151e4561127d59d29f6495c29b74dce247637
emsvip <93297386+emsvip@users.noreply.github.com>
2023-03-17 14:59:35 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -64,7 +64,6 @@ openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
 Lede-x86-64-generic-squashfs-combined.img.gz
 Lede-x86-64-generic-squashfs-combined-efi.img.gz
-x86-64-generic-squashfs-combined.qcow2
 sha256sums
 version.buildinfo
 ipk.tar.gz
